d74a6a2e0e134e8c33fa1241073235625fc60e80
joseacunagarro <124007699+joseacunagarro@users.noreply.github.com>
2024-12-13 11:06:04 -0600

    Clase 13 Finalizada

--- src/main/java/com/tienda/controller/UsuarioController.java
+++ src/main/java/com/tienda/controller/UsuarioController.java
@@ -1,20 +1,21 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.tienda.controller;
 
-import com.tienda.domain.Usuario; import com.tienda.service.UsuarioService; import com.tienda.service.FirebaseStorageService; import org.springframework.beans.factory.annotation.Autowired; import org.springframework.stereotype.Controller; import org.springframework.ui.Model; import org.springframework.web.bind.annotation.GetMapping; import org.springframework.web.bind.annotation.PostMapping; import org.springframework.web.bind.annotation.RequestMapping; import org.springframework.web.bind.annotation.RequestParam; import org.springframework.web.multipart.MultipartFile;
-/**
- *
- * @author jose.a.acuna.garro
- */
+import com.tienda.domain.Usuario;
+import com.tienda.service.FirebaseStorageService;
+import com.tienda.service.UsuarioService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
 @Controller
 @RequestMapping("/usuario")
-
 public class UsuarioController {
-    
+
     @Autowired
     private UsuarioService usuarioService;
 
@@ -36,7 +37,7 @@ public class UsuarioController {
 
     @PostMapping("/guardar")
     public String usuarioGuardar(Usuario usuario,
-            @RequestParam("imagenFile") MultipartFile imagenFile) {
+                                 @RequestParam("imagenFile") MultipartFile imagenFile) {
         if (!imagenFile.isEmpty()) {
             usuarioService.save(usuario,false);
             usuario.setRutaImagen(
@@ -61,5 +62,4 @@ public class UsuarioController {
         model.addAttribute("usuario", usuario);
         return "/usuario/modifica";
     }
-    
 }
